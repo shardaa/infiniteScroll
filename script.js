65d6cdc51e1d1b59976e2dc04fd59279cec5a289
Ashish sharda <shardaashish097@gmail.com>
2022-03-10 00:50:08 +0530

    infinite scroll code added

---
+++ script.js
@@ -0,0 +1,78 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById("loader");
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = []
+
+const apiKey = "Fc7Q0ZIl328-4boi3YV4Wfwo-yeoqLd3zY_AgKSZSmg";
+const count = 30;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+
+function imageLoaded() {
+     imagesLoaded++;
+     if (imagesLoaded === totalImages) {
+         ready = true;
+         loader.hidden = true;
+     }
+}
+
+function setAttributes(el, attributes) {
+    for (const key in attributes) {
+        el.setAttribute(key, attributes[key]);
+    }
+}
+
+function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    photosArray.forEach((photo) => { 
+        
+        // Create LInk
+        const item = document.createElement('a')
+        // item.setAttribute('href',photo.links.html)
+        // item.setAttribute('target',"_blank")
+        setAttributes(item,{
+            href:photo.links.html,
+            target:"_blank",
+        })
+        // Create Photo
+        const img = document.createElement('img')
+        // img.setAttribute('src',photo.urls.regular) 
+        // img.setAttribute('alt',photo.alt_description) 
+        // img.setAttribute('title',photo.alt_description) 
+        setAttributes(img,{
+            src:photo.urls.regular,
+            alt:photo.alt_description,
+            title:photo.alt_description,
+        })
+
+        img.addEventListener('load', imageLoaded)
+
+        
+        item.appendChild(img)
+        imageContainer.appendChild(item)
+    });
+  
+}
+
+async function fetchImage() {
+    try {
+        let response = await fetch(apiUrl)
+        photosArray = await response.json();
+       displayPhotos()
+    } catch (error) {
+        
+    }
+}
+
+window.addEventListener("scroll", () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        fetchImage();
+       
+    }
+})
+fetchImage()
